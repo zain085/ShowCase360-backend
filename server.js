6d5fcd367b357b654656a080a1c9089f56fe2cc4
Zain Ahmed <zainahmedkhan085@gmail.com>
2025-08-17 17:34:21 +0500

    adding netlify link

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5173", 
+    // "http://localhost:5173", 
     "https://showcase360.netlify.app/" 
   ],
   credentials: true
